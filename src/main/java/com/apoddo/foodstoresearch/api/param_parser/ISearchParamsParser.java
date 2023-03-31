e581ce2e0f1865cbf72d59f0c67227cde557db90
Strahinja <keseljstrahinja@gmail.com>
2023-03-31 12:24:55 +0200

    fixed spacing

--- src/main/java/com/apoddo/foodstoresearch/api/param_parser/ISearchParamsParser.java
+++ src/main/java/com/apoddo/foodstoresearch/api/param_parser/ISearchParamsParser.java
@@ -3,7 +3,6 @@ package com.apoddo.foodstoresearch.api.param_parser;
 import com.apoddo.foodstoresearch.api.param_parser.model.SearchParams;
 
 public interface ISearchParamsParser {
-
     SearchParams parse(String latitude, String longitude, String distance, String batchSize);
     SearchParams parse(String partialQuery, String batchSize);
     double parseLatitude(String latitude);
